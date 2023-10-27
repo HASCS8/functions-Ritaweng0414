e1c67c5a70bcc6bf5b7c6cd248fd3c7c93edf84f
rita <rita.weng@has.hc.edu.tw>
2023-10-27 12:56:28 +0800

    hi

--- solutions/functions.py
+++ solutions/functions.py
@@ -2,35 +2,42 @@ def add(a, b):
     """
     Return the sum of a and b.
     """
+    return a + b
+    print(a, b)
     pass
 
 def subtract(a, b):
     """
     Return the result of a minus b.
     """
+    return  a - b
+    print(a, b)
     pass
 
 def multiply(a, b):
     """
     Return the product of a and b.
     """
-    return a + b
+    return a * b
 
 def divide(a, b):
     """
     Return the result of a divided by b.
     If b is zero, return None.
     """
+    return a / b
     pass
 
 def power(a, b):
     """
     Return the result of raising a to the power of b.
     """
+    return a ** b
     pass
 
 def reverse_string(s):
     """
-    Return the string s reversed. Such that if s is 'hello' then the function should return 'olleh'
+    
     """
-    pass
+    return a + b
+    pass   
